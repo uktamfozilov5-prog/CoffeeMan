1d1b745c46bdc0dad7a2a09c589255ed85aa938c
uktamfozilov5-prog <uktamfozilov5@gmail.com>
2026-06-02 20:08:04 +0500

    Sayt tayor

--- src/companent/Button.jsx
+++ src/companent/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ label }) => {
   return (
-    <div className="button"> My Button</div>
+    <button className="button">{label}</button>
   )
 }
 
--- src/companent/Navbar.jsx
+++ src/companent/Navbar.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import logo from '../assets/logo.svg'
+import hero from '../assets/Group 22.png'
 import Button from './Button'
+
 const Navbar = () => {
   return (
-    <nav className='navbar'>
-      <img src={logo} alt="Logo" />
-      <Button />
-    </nav>
+    <header className="hero" style={{ backgroundImage: `url(${hero})` }}>
+      <div className="hero-overlay">
+        <div className="container">
+          <nav className="navbar">
+            <div className="brand">
+              <img src={logo} alt="CoffeeScan logo" />
+            </div>
+            <Button label="Launch App" />
+          </nav>
 
+          <div className="hero-content">
+            <div className="hero-copy">
+              <span className="hero-line" />
+              <p className="subtitle">SIMPLY CLEVER</p>
+              <h1 className="title">BEST COFFEE</h1>
+              <p className="description">Lorem Ipsum Dolor met sit dolor</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
